ee816ba4988de69dd44373f0eff7814f87760395
Anmeet <anmeet619@yahoo.com>
2022-12-30 15:20:41 +1100

    nextjs practice app

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import EventList from '../components/events/event-list'
+import { getFeaturedEvents } from '../helpers/api-util'
+import NewsletterRegistration from '../components/input/newsletter-registration'
+
+function HomePage({ events }) {
+  return (
+    <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name='description' content='Find a lot of great events' />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={events} />
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents()
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  }
+}
+
+export default HomePage
